b551cd269705072225e9dd5811ea36b1e9af6e3d
Dody <dodysenz@gmail.com>
2019-06-22 20:52:55 +0800

    Bug Fix on Authentication_bloc

--- lib/blocs/authentication_bloc/authentication_bloc.dart
+++ lib/blocs/authentication_bloc/authentication_bloc.dart
@@ -7,7 +7,6 @@ import 'package:k_project_dodyversion/blocs/authentication_bloc/authentication_s
 import 'package:k_project_dodyversion/models/user_model.dart';
 export 'package:k_project_dodyversion/blocs/authentication_bloc/authentication_state.dart';
 import 'package:k_project_dodyversion/resources/repository.dart';
-import 'package:k_project_dodyversion/resources/user_repo/user_provider.dart';
 
 class AuthenticationBloc
     extends Bloc<AuthenticationEvent, AuthenticationState> {
@@ -27,9 +26,6 @@ class AuthenticationBloc
     if (event is LoggingInGoogleAccountAuthenticationEvent) {
       yield* mapLoggingInGoogleAccountAuthenticationEventToState(event);
     }
-    // } else if (event is LoggingInEmailAuthenticationEvent) {
-    //   yield* mapLoggingInEmailAuthenticationEventToState(event);
-    // }
   }
 
   Stream<AuthenticationState>
@@ -53,30 +49,4 @@ class AuthenticationBloc
     }
   }
 
-  // Stream<AuthenticationState> mapLoggingInEmailAuthenticationEventToState(
-  //     LoggingInEmailAuthenticationEvent event) async* {
-  //   yield LoggingInEmailState(); // Notifying others that the I am reaching the server
-  //   try {
-  //     // Try to authenticate with current email and password
-  //     // await _firebaseRepository.authenticateUserEmail(
-  //     //   email: event.emailAuth.email,
-  //     //   password: event.emailAuth.password,
-  //     // );
-
-  //     yield LoggedInState(); // Telling that the BloC has successfully authenticating the user
-  //   } catch (e) {
-  //     // If there is an error(either not registered, or wrong password)
-  //     print(e.toString());
-  //     try {
-  //       await _firebaseRepository.registerUser(
-  //         email: event.emailAuth.email,
-  //         password: event.emailAuth.password,
-  //       );
-  //       yield LoggedInState();
-  //     } catch (e) {
-  //       // If there is an error here, Either the guy has registered but forgot the password or he is trying to make multiple account.
-  //       print("sadkjfahsahsdlfkjashldkfj ${e.toString()}");
-  //     }
-  //   }
-  // }
 }
